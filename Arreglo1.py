5af28893275d653d2fc663e9035d023cfb731a95
garcia-lopez <maria.lopez14959@est.uca.edu.ni>
2022-08-29 09:48:40 -0600

    Agregue un ejemplo de arreglo

---
+++ Arreglo1.py
@@ -0,0 +1,12 @@
+numeros = []
+numeros.append(3)
+numeros.append(8)
+numeros.append(9)
+numeros.append(20)
+print("Elementos del arreglo: ")
+print(numeros)
+numeros.insert(1, 15)   #pide la posicion y el  numero a ubicar
+print(numeros)
+
+numeros.remove(3) #elimina el elemento
+print(numeros)
